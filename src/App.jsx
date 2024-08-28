e6920a555f4cf957ffbb62e5dc7ca4ea0524e769
TechnicalParadox <giamolao98@gmail.com>
2024-08-28 09:17:55 -0400

    Moved themeprovider to app.jsx for simplicity

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme'
+
 import { CssBaseline, Typography } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider theme={theme} >
       <CssBaseline/>
       <Typography variant='h5'>Hello from React + Vite + Material UI v6 🚀</Typography>
-    </>
+    </ThemeProvider>
   )
 }
 
